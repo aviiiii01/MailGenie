afbc256a9a9b3d6098097d676b70a817b2a1c1ac
aviiiii01 <avinashlodhi58@gmail.com>
2025-03-30 22:51:25 +0530

    All the things done now webapp is ready! to use

--- mailbot.py
+++ mailbot.py
@@ -9,6 +9,7 @@ from langchain.embeddings import HuggingFaceBgeEmbeddings
 # from langchain.chains import ConversationalRetrievalChain
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
+import re
 
 
 
@@ -26,6 +27,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 st.set_page_config("Mail Botty!")
 st.header("Write the context of the mail And the Mails of the Recievers")
+ml=st.text_input("Enter your mail...! ")
+passw=st.text_input("Enter your app password (go to manage your account and create 'app password'):", type="password")
 context=st.text_input("Write the Context of the mail....")
 maill=st.text_input("Write the mails of the recievers here separated with comma ','.....")
 tone=st.radio("Select Email tone: ",["Proffesional","Friendly"])
@@ -40,6 +43,7 @@ tone=st.radio("Select Email tone: ",["Proffesional","Friendly"])
 #     embeddings = HuggingFaceBgeEmbeddings(model_name="all-MiniLM-L6-v2")
 #     vectorstore=FAISS.from_texts(texts=chunks,embedding=embeddings)
 llm=ChatGoogleGenerativeAI(model="gemini-2.0-pro-exp-02-05",api_key=GEMINI_API_KEY,temperature=0.7)#"gemini-2.0-flash"
+
 # qa_chain=ConversationalRetrievalChain.from_llm(
 #     llm=llm,
 #     retriever=vectorstore.as_retriever(),
@@ -49,18 +53,22 @@ llm=ChatGoogleGenerativeAI(model="gemini-2.0-pro-exp-02-05",api_key=GEMINI_API_K
 # response = qa_chain({"question": context})
 # st.write(response["answer"])
 # st.write(maill)
-prompt = f"Generate an email with only the subject and body for a {tone.lower()} email based on the following context:\n{context}\nFormat: \nSubject: <Subject>\nBody: <Body>"
+
+prompt = f"Generate an email with only the subject, body, salutation and Closing/Complimentary Closing for a {tone.lower()} email based on the following context:\n{context}\nFormat: \nSubject: <Subject>\nBody: <Body>"
 response=llm.invoke(prompt)
 st.subheader("Generated Email Content:")
-st.write(response)
-
+st.write(response.content)
+email_content = str(response.content)
 
 lst=maill.split(',')
 SENDER_MAIL=os.getenv("SENDER_MAIL")
-sender=SENDER_MAIL
-password="jibs ohun hder uxly"
-subject="Hello Girllsss!!!!!"
-body = str(response.content)
+sender=ml
+password=passw
+subject_match = re.search(r"Subject:\s*(.+)", email_content)
+body_match = re.search(r"Body:\s*(.+)", email_content, re.DOTALL)
+
+subject = subject_match.group(1).strip() if subject_match else "No Subject"
+body = body_match.group(1).strip() if body_match else email_content.strip()
 
 for i in lst:
     x=EmailMessage()
